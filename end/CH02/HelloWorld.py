5c5e1b0b96301b1b0708dd3d96ee19a5508bf3d1
MJ <mahmad2@mix.wvu.edu>
2022-08-29 16:01:51 -0400

    I just added a new command.

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created by Ed Goad, 2/3/2021
-print("Hello World")
+print('Hello World')
 print('MJ Ahmad test')
+print('CYBR493')
+print('Do you see this?')
